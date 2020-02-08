b5d848e764df2aabff4cc4344de3a0341769515d
venustuspl <venustus.pl@gmail.com>
2020-02-08 07:55:33 +0100

    0.01

---
+++ src/main/java/pl/venustus/Tymianek/TymianekApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.Tymianek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymianekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TymianekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/Tymianek/TymianekApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.Tymianek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymianekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
